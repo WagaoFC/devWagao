bc9b2cb6f296072748dbe5296221fd4ff3944b90
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-01 14:55:49 -0300

    acrescentando state

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,18 +1,18 @@
+import { useState } from 'react'
 import { FaTools } from 'react-icons/fa'
 import { MdEmojiPeople } from 'react-icons/md'
-import { AiFillHtml5 } from 'react-icons/ai'
-import { IoLogoCss3 } from 'react-icons/io'
-import { SiJavascript } from 'react-icons/si'
 import { Container } from './styled'
 
 export function Skills() {
+    const [changeSkill, setChangeSkill] = useState('soft');
+
     return (
         <Container>
-            <button>
+            <button onClick={() => setChangeSkill('soft')}>
                 <MdEmojiPeople size="25" />
                 Soft Skills
             </button>
-            <button>
+            <button onClick={() => setChangeSkill('hard')}>
                 <FaTools size="20" />
                 Hard Skills
             </button>
