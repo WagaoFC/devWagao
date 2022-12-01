6b5f83b4229c0ecb5113789448aa9ee49c3a96a4
Wagner Ferreira Costa <wagner.costa@zema.com>
2022-12-01 14:28:07 -0300

    adicionando shadow e transition ao componente skills

--- src/components/Skills/styled.ts
+++ src/components/Skills/styled.ts
@@ -14,15 +14,17 @@ export const Container = styled.div`
         border: solid 2px ${props => props.theme.title === 'light' ? 'var(--background-dark)' : 'var(--background-light)'};
         background-color: ${props => props.theme.title === 'light' ? 'var(--background-dark)' : 'var(--background-light)'};
         color: ${props => props.theme.title === 'light' ? 'var(--text-color-light)' : 'var(--text-color-dark)'};
-
-        svg {
-            margin: 0;
-        }
+        box-shadow: 0 15px 15px rgba(0, 0, 0, 0.2);
+        transition: color 0.1s, background-color 0.3s;
 
         &:hover {
             background-color: ${props => props.theme.title === 'light' ? 'var(--background-light)' : 'var(--background-dark)'};
             color: ${props => props.theme.title === 'light' ? 'var(--background-dark)' : 'var(--background-light)'};
             border: solid 2px ${props => props.theme.title === 'light' ? 'var(--background-dark)' : 'var(--background-light)'};
+        }
+        
+        svg {
+            margin: 0;
         }
     }
 `
