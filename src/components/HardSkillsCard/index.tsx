55dddc42e35368c295c071beaeeb414a1a30d592
WagaoFC <wagnerferreira_07@hotmail.com>
2023-03-19 08:54:37 -0300

    :sparkles: feat: add framer-motion

--- src/components/HardSkillsCard/index.tsx
+++ src/components/HardSkillsCard/index.tsx
@@ -12,47 +12,120 @@ import {
     SiVisualstudiocode,
     SiCsharp
 } from 'react-icons/si'
+import { motion } from 'framer-motion'
 import { Cards, Container } from "./styled";
 
 export function HardSkillsCard() {
     return (
             <Container>
-                <Cards>
-                    <SiHtml5 />
-                </Cards>
-                <Cards>
-                    <SiCss3 />
-                </Cards>
-                <Cards>
-                    <SiJavascript />
-                </Cards>
-                <Cards>
-                    <SiReact />
-                </Cards>
-                <Cards>
-                    <SiTypescript />
-                </Cards>
-                <Cards>
-                    <SiStyledcomponents />
-                </Cards>
-                <Cards>
-                    <SiCsharp />
-                </Cards>
-                <Cards>
-                    <SiNodedotjs />
-                </Cards>
-                <Cards>
-                    <SiMicrosoftsqlserver />
-                </Cards>
-                <Cards>
-                    <SiGit />
-                </Cards>
-                <Cards>
-                    <SiGithub />
-                </Cards>
-                <Cards>
-                    <SiVisualstudiocode />
-                </Cards>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 120 }}
+                >
+                    <Cards>
+                        <SiHtml5 />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 110 }}
+                >
+                    <Cards>
+                        <SiCss3 />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 100 }}
+                >
+                    <Cards>
+                        <SiJavascript />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 90 }}
+                >
+                    <Cards>
+                        <SiReact />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 80 }}
+                >
+                    <Cards>
+                        <SiTypescript />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 70 }}
+                >
+                    <Cards>
+                        <SiStyledcomponents />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 120 }}
+                >
+                    <Cards>
+                        <SiCsharp />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 110 }}
+                >
+                    <Cards>
+                        <SiNodedotjs />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 100 }}
+                >
+                    <Cards>
+                        <SiMicrosoftsqlserver />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 90 }}
+                >
+                    <Cards>
+                        <SiGit />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 80 }}
+                >
+                    <Cards>
+                        <SiGithub />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 70 }}
+                >
+                    <Cards>
+                        <SiVisualstudiocode />
+                    </Cards>
+                </ motion.div>
             </Container>
     )
 }
--- src/components/SoftSkillsCard/index.tsx
+++ src/components/SoftSkillsCard/index.tsx
@@ -1,26 +1,57 @@
 import { FaPeopleCarry, FaUserAstronaut} from 'react-icons/fa'
 import { GiTalk } from 'react-icons/gi'
 import { RiEmotionFill } from 'react-icons/ri'
+import { motion } from 'framer-motion'
 import { Cards, Container } from "./styled";
 
 export function SoftSkillsCard() {
     return (
             <Container>
-                <Cards>
-                    <FaPeopleCarry />
-                </Cards>
-                <Cards>
-                    <GiTalk />
-                </Cards>
-                <Cards>
-                    <RiEmotionFill />
-                </Cards>
-                <Cards>
-                    <FaUserAstronaut />
-                </Cards>
-                <Cards>
-                    <FaUserAstronaut />
-                </Cards>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 120 }}
+                >
+                    <Cards>
+                        <FaPeopleCarry />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 110 }}
+                >
+                    <Cards>
+                        <GiTalk />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 100 }}
+                >
+                    <Cards>
+                        <RiEmotionFill />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 90 }}
+                >
+                    <Cards>
+                        <FaUserAstronaut />
+                    </Cards>
+                </ motion.div>
+                <motion.div 
+                    initial={{ y: "100%" }}
+                    animate={{ y: 0 }}
+                    transition={{ type: "spring", stiffness: 80 }}
+                >
+                    <Cards>
+                        <FaUserAstronaut />
+                    </Cards>
+                </ motion.div>
             </Container>
     )
 }
